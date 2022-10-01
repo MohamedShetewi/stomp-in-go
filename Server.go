ad03dc972b341dfd10cf76541b77d7c57eab6798
Mohamed Shetewi <Muhamedshetewi@gmail.com>
2022-10-01 22:14:02 +0200

    Added Server struct

---
+++ Server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+const (
+	HOST = "localHost"
+	PORT = "3000"
+	TYPE = "tcp"
+)
+
+type Server struct {
+}
+
+func (server *Server) init() {
+	tcpServer, err := net.Listen(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		connection, err := tcpServer.Accept()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		go server.handleConnection(&connection)
+	}
+}
+
+func (server *Server) handleConnection(connection *net.Conn) {
+
+}
